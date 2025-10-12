156e2f5401e1059e4b038c530cc73eed88b5519a
Patricia Felber <patricia.felber@t-online.de>
2025-10-12 12:57:24 +0200

    structure

--- PetCheck/src/app/app.routes.ts
+++ PetCheck/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+            },
+            {
+                path: 'calendar',
+                loadChildren: () => import('./features/calendar/calendar.module').then(m => m.CalendarModule)
+            },
+            {
+                path: 'settings',
+                loadChildren: () => import('./features/settings/settings.module').then(m => m.SettingsModule)
+            },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+        ]
+    }
+];
