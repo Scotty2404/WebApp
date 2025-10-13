f619d792a9f25300ca5b0c3e92cab345b1ae84b2
Patricia Felber <patricia.felber@t-online.de>
2025-10-13 15:20:53 +0200

    add store

--- PetCheck/src/app/app.config.ts
+++ PetCheck/src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    }), provideStore()]
+    }), provideStore(), provideEffects()]
 };
